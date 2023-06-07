1897688b5432cffa1ba51cacf17b84e60773fb6b
Ifeanyi Osuji <oindubuisi@gmail.com>
2023-06-07 15:42:32 +0100

    using named placeholders and conditional specifiers

--- string-manipulation-basics/string-formating.py
+++ string-manipulation-basics/string-formating.py
@@ -1,4 +1,4 @@
-
+# Using placeholders
 wikipedia_article = 'In computer science, artificial intelligence (AI), sometimes called machine intelligence, is intelligence demonstrated by machines, in contrast to the natural intelligence displayed by humans and animals.'
 """
 Assign the substrings going from the 4th to the 19th character inclusive, and from the 22nd to the 44th character inclusive of wikipedia_article to the variables first_pos and second_pos, respectively. 
@@ -48,3 +48,44 @@ my_list.append("The tool {1} is used in {0}")
 # Use format to print strings
 for my_string in my_list:
   	print(my_string.format(first_pos, second_pos))
+	
+#  Using named PLACEHOLDERS
+courses = ['artificial intelligence', 'neural networks']
+"""
+Create a dictionary assigning the first and second element appearing in the list courses to the keys "field" and "tool" respectively.
+"""
+# Create a dictionary
+plan = {
+  		'field': courses[0],
+        'tool': courses[1]
+        }
+
+"""
+    Complete the placeholders accessing inside to the elements linked with the keys field and tool in the dictionary data.
+    Print out the resulting message using the .format() method, passing the plan dictionary to replace the data placeholders.
+"""
+# Complete the placeholders accessing elements of field and tool keys in the data dictionary
+my_message = "If you are interested in {data[field]}, you can take the course related to {data[tool]}"
+
+# Use the plan dictionary to replace placeholders
+print(my_message.format(data=plan))
+
+
+# Using named placeholders and format specifiers
+"""
+    Import the function datetime from the module datetime .
+    Obtain the date of today and assign it to the variable get_date.
+    Complete the string message by adding to the placeholders named today and the format specifiers to format the date as month_name day, year and time as hour:minutes.
+    Print the message using the .format() method and the variable get_date to replace the named placeholder.
+"""
+# Import datetime 
+from datetime import datetime
+
+# Assign date to get_date
+get_date = datetime.now()
+
+# Add named placeholders with format specifiers
+message = "Good morning. Today is {today:%B %d, %Y}. It's {today:%H:%M} ... time to work!"
+
+# Use the format method replacing the placeholder with get_date
+print(message.format(today=get_date))

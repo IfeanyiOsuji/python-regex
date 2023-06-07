9aa865f681282a3e0f9b558d781ed66ec22a0f03
Ifeanyi Osuji <oindubuisi@gmail.com>
2023-06-07 13:48:57 +0100

    selecting substrings in a string

--- string-manipulation-basics/basic_string_functions.py
+++ string-manipulation-basics/basic_string_functions.py
@@ -3,3 +3,12 @@ movie = 'fox and kelley soon become bitter rivals because the new fox books stor
 
 #find the length
 print(len(movie))
+
+# Select the first 32 characters of the variable movie1 and assign it to the variable first_part
+first_part = movie[:32]
+print(first_part)
+
+movie1 = 'the most significant tension of _election_ is the potential relationship between a teacher and his student .'
+# Select the substring going from the 43rd character to the end of movie1. Assign it to the variable last_part.
+last_part = movie1[42:]
+print(last_part)

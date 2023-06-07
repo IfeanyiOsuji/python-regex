88e23b0e5978e21214603df2d9ea80d2aad00834
Ifeanyi Osuji <oindubuisi@gmail.com>
2023-06-07 13:37:21 +0100

    finding the size of a string'

---
+++ string-manipulation-basics/basic_string_functions.py
@@ -0,0 +1,5 @@
+
+movie = 'fox and kelley soon become bitter rivals because the new fox books store is opening up right across the block from the small business .'
+
+#find the length
+print(len(movie))
